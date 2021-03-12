338c18db8baf9cf15f4008e0fb3e94d9e5b24463
kayake <77542672+kayke981@users.noreply.github.com>
2021-03-12 19:11:26 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+//dependências
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    shardCount: 1
+    });
+//clients
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+//arquivos
+const config = require('./config.json');
+require('./src/reply/reply.js');
+
+const handler = require('./handler/handler.js')
+handler.loadCommand(client.commands, client.aliases, 'comando de nº{number} carregado: ${cmd}')
+
+//carregar os arquivos aqui
+require('./loader/event.js')(client)
+
+require('./loader/db.js')(client)
+
+//https://github.com/yADGithub foi onde eu peguei o handler de eventos e a db.js :)
+client.login(config.token)
