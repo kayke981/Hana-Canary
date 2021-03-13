e3224a68dcba9c1cf1778774b4398889ffc6f4ed
kayake <77542672+kayke981@users.noreply.github.com>
2021-03-12 21:43:46 -0300

    Update messageCommand.js

--- src/events/messageCommand.js
+++ src/events/messageCommand.js
@@ -1,12 +1,17 @@
 const config = require('../../config.json');
 const Discord = require('discord.js');
+const stringS = require('string-similarity');
 
 module.exports = (client) => {
 client.on('message', async (message) => { 
 if (message.author.bot) return;    
-if (message.channel.type == 'dm') return;   
+if (message.channel.type == 'dm') return;
 if (!message.content.toLowerCase().startsWith(config.prefix)) return;
     //verificar se tomou ban no bot
+    
+    const cmds = message.content.split(' ')[0].slice(config.prefix.length)
+        const {bestMatch} = stringS.findBestMatch(cmds, client.commands.map((a) => a.config.name))
+        
     let botban = await client.db.get(`blacklist`)
     let motivo = await client.db.get(`motivo`)
     //quando n tiver IDs, tem q colocar qualquer um ID, pode ser até mesmo do bot
@@ -20,10 +25,14 @@ const args = message.content        .trim()        .slice(config.prefix.length) 
 const command = args.shift().toLowerCase();   
 const commandFile = client.commands.get(command) || client.commands.get(client.aliases.get(command));
     
+     if(!cmds) return;
+            if(cmds.includes("@everyone")) return;
+            if(cmds.includes('`')) return;
+            if(!commandFile) return message.channel.send(`o comando \`${cmds}\` não existe, mas você não quis dizer \`${bestMatch.target}\``)
 if(commandFile) {
     
     const embed = new Discord.MessageEmbed()
-    .setAuthor(`Log de comandos | ${message.guild.name}(${message.author.id})`, message.guild.iconURL())
+    .setAuthor(`Log de comandos | ${message.guild.name}(${message.guild.id})`, message.guild.iconURL())
     .setDescription(`**Usuário:**\n\`${message.author.tag}(${message.author.id})\`\n**Comando usado**\n\`${message.content}\``)
     .setFooter(`ID ${message.author.id}`)
     .setTimestamp()
