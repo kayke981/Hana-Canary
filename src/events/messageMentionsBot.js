3cea34c4d1dd2932b7fc192f3c27ecb4380363a1
kayake <77542672+kayke981@users.noreply.github.com>
2021-03-13 14:09:46 -0300

    colocando para o bot n responder a dm :)

--- src/events/messageMentionsBot.js
+++ src/events/messageMentionsBot.js
@@ -1,15 +1,17 @@
-const {MessageEmbed} = require('discord.js');
+const Discord = require('discord.js');
 const config = require('../../config.json');
 
 
 module.exports = (client) => {
   client.on('message', (message) => {
+    if(message.channel.type == 'dm') return;
+    if(message.author.bot) return;
   if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) {
     const embed = new MessageEmbed()
     .setTitle('<:paste:794205638120570911> <a:SH_da_seta:783516413448159313> Prefixo')
     .setDescription(`<:user:793984556197740606> <a:SH_da_seta:783516413448159313> Olá ${message.author}!\n
 <:information:793890482068914228> <a:SH_da_seta:783516413448159313> Meu prefixo é \`${config.prefix}\``)
-    .setColor('#F27BDA')
+    .setColor(config.color)
     message.channel.send(embed);
   }
   })
