013276e0fde0627e3e483c6eb678ee6781f1d576
kayake <77542672+kayke981@users.noreply.github.com>
2021-03-13 14:10:36 -0300

    Esqueci de outra coisa kk

--- src/events/messageMentionsBot.js
+++ src/events/messageMentionsBot.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
     if(message.channel.type == 'dm') return;
     if(message.author.bot) return;
   if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) {
-    const embed = new MessageEmbed()
+    const embed = new Discord.MessageEmbed()
     .setTitle('<:paste:794205638120570911> <a:SH_da_seta:783516413448159313> Prefixo')
     .setDescription(`<:user:793984556197740606> <a:SH_da_seta:783516413448159313> Olá ${message.author}!\n
 <:information:793890482068914228> <a:SH_da_seta:783516413448159313> Meu prefixo é \`${config.prefix}\``)
