0fa5697476c8a98f8c83584dc6fe11e947ceb248
kayake <77542672+kayke981@users.noreply.github.com>
2021-03-19 19:33:15 -0300

    COMEÇANDO PAEEEEEEEEE

---
+++ src/events/messageDm.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const config = require('../../config.json')
+
+module.exports = async (client) => {
+    client.on('message', async (message) => {
+    if(message.guild) return;
+    
+    const DM = new Discord.MessageEmbed()
+    .setAuthor(`Log de dms | ${message.author.tag}(${message.author.id})`, message.author.avatarURL())
+ .setDescription(`**mensagem**\n\`${message.content}\``)
+    .setFooter(`ID ${message.author.id}, tag ${message.author.tag}`)
+    .setTimestamp()
+    .setColor(config.color)
+    client.channels.cache.get("821547129859604490").send(DM)
+    });
+    }
