38f5dd24a0bb7a11ff2462d73d94dddcbb41c51e
LuisAntonyᵇʳᶻˡ <72362837+luizzitow123@users.noreply.github.com>
2021-03-23 14:53:45 -0300

    Melhorias em messageDM.js

--- src/events/messageDm.js
+++ src/events/messageDm.js
@@ -3,15 +3,15 @@ const config = require('../../config.json')
 
 module.exports = async (client) => {
     client.on('message', async (message) => {
-    if(message.guild) return;
+    if(message.channel.type !== "DM") return;
     
     const DM = new Discord.MessageEmbed()
     .setAuthor(`Log de dms | ${message.author.tag}(${message.author.id})`, message.author.avatarURL())
- .setDescription(`**mensagem**\n\`${message.content}\``)
+ .setDescription(`**mensagem**\n\`||${message.content}||\``)
     .setFooter(`ID ${message.author.id}, tag ${message.author.tag}`)
     .setTimestamp()
     .setColor(config.color)
-    client.channels.cache.get("id do canal").send(DM)
+    client.channels.cache.get("id").send(DM)
     });
     }
 //n recomendo por causa do MASS DM
