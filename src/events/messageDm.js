62fa65450c643eee890924a3241239f51897bc09
LuisAntonyᵇʳᶻˡ <72362837+luizzitow123@users.noreply.github.com>
2021-04-10 18:10:26 -0300

    criar um anti-massDM
    tá mais pra anti-spam mas ok ;-;

--- src/events/messageDm.js
+++ src/events/messageDm.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
-const config = require('../../config.json')
-
+const config = require('../../config.json');
+const lastMessages = new Map();
 module.exports = async (client) => {
     client.on('message', async (message) => {
     if(message.channel.type !== "DM") return;
+        
+    const lastMessage = lastMessages.get('lastMessage');
+        
+    if(lastMessages.has('lastMessage')) {
+    if(lastMessage.content === message.content) return;
+    }
     
     const DM = new Discord.MessageEmbed()
     .setAuthor(`Log de dms | ${message.author.tag}(${message.author.id})`, message.author.avatarURL())
@@ -11,7 +17,11 @@ module.exports = async (client) => {
     .setFooter(`ID ${message.author.id}, tag ${message.author.tag}`)
     .setTimestamp()
     .setColor(config.color)
-    client.channels.cache.get("id").send(DM)
+    client.channels.cache.get("id").send(DM);
+        
+    lastMessages.set('lastMessage', {
+    content: message.content
+    })
     });
     }
 //n recomendo por causa do MASS DM
