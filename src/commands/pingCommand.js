773b465bfc918f188d7a1478ebabed4397ac6df9
kayake <77542672+kayke981@users.noreply.github.com>
2021-04-10 16:56:45 -0300

    atualizando e deixando mais bonito

--- src/commands/pingCommand.js
+++ src/commands/pingCommand.js
@@ -7,8 +7,8 @@ exports.run = async (client, message, args) => {
     
     message.inlineReply('Ping?').then(msg => {
     
-    msg.edit(`🏓 **|** ${message.author} pong\n:satellite: **|** **Shards:**(${message.guild.shard.id}/${config.shards})\n:stopwatch: **|** Latência da API: ${svPing}\n:zap: **|** Ping: ${client.ws.ping}`)
-        });
+    msg.edit(`🏓 **|** ${message.author} pong\n:satellite: **|** **Shards:**(${message.guild.shard.id}/${config.shards})\n:stopwatch: **|** Latência da API: ${svPing}\n:zap: **|** Ping: ${client.ws.ping}\n🌐 **|** Ping da DataBase:\n📖 **|** ${ping.read}ms read\n✏ **|** ${ping.write}ms write\n🌏 **|** Ping da shard: ${shardPing}ms`) 
+});
     }
 exports.config = {
     name: 'ping',
