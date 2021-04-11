dfa6d3adb566ad9f80983f5d8f144c195ca46c12
LuisAntonyᵇʳᶻˡ <72362837+luizzitow123@users.noreply.github.com>
2021-04-10 21:43:19 -0300

    transformar config.json em um painel
    tomara que esteja tudo certo

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,13 +1,24 @@
+const config = require('../../config.json');
 const Discord = require('discord.js');
 module.exports = (client) => {
 client.on('guildMemberAdd', async (member) => {
+    const peaceHolders = {
+    '{member}': member.user.username,
+    '{@member}': member.user.toString(),
+    '{guild}': member.guild.name,
+    '{guild.id}': member.guild.id,
+    '{memberCount}': member.guild.members.filter(m => m.user.bot !== true).size,
+    '{botCount}': member.guild.members.filter(m => m.user.bot !== false).size
+    }
+    
+    const peaceReq = new RegExp(Object.keys(peaceHolders).join('|'), 'ig')
     let channel = await client.db.get(`welcome-${member.guild.id}`)
     
     if(channel === null) return;
     
     const embed = new Discord.MessageEmbed()
     .setTitle(`**Bem vindo**`)
- .setDescription(`**${member.user}\`(${member.user.tag})\`Bem vindo ao servidor ${member.guild.name}, atualmente estamos com ${member.guild.memberCount} membros**`)
+ .setDescription(config.frases.welcome.replace(peaceReq, match => peaceHolders[match]))
     
     client.channels.cache.get(channel).send(embed)
     });
