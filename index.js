02376a2cf8c32fb7c3b1f4544a4dc3670cb27ce0
kayake <77542672+kayke981@users.noreply.github.com>
2021-04-15 20:56:19 -0300

    Nova index

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 //dependências
 const Discord = require('discord.js');
-const client = new Discord.Client()
+const client = new Discord.Client({
+
+//restTimeOffset: 1,
+//ws: {
+   //intents: 1539
+// }
+    
+});
+const { Player } = require("discord-player")
+const player = new Player(client);
 //clients
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+client.player = player;
+client.cooldown = new Discord.Collection();
 
 //arquivos
 const config = require('./config.json');
 require('./src/reply/reply.js');
 
+
 const handler = require('./handler/handler.js')
+
 handler.loadCommand(client.commands, client.aliases, 'comando de nº{number} carregado: ${cmd}')
 
 //carregar os arquivos aqui
@@ -17,5 +30,9 @@ require('./loader/event.js')(client)
 
 require('./loader/db.js')(client)
 
+require('./hanaPlayers/player.js')(client)
+
 //https://github.com/yADGithub foi onde eu peguei o handler de eventos e a db.js :)
-client.login(config.token)
+client.login(config.token).catch(err => {
+    console.log(err.code)
+    })
