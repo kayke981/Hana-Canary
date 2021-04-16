a749f9c5f1ac55b728cf4d263e7a4a79739a9d65
kayake <77542672+kayke981@users.noreply.github.com>
2021-04-16 14:57:39 -0300

    vdd

--- src/events/messageCommand.js
+++ src/events/messageCommand.js
@@ -43,7 +43,7 @@ timeout: Date.now() + 3210
         }
     
     if(botban.includes(message.author.id)) return;
-    if(!client.cooldown.has(message.author.id).timeout) {
+    if(!client.cooldown.get(message.author.id).timeout) {
 const args = message.content        .trim()        .slice(config.prefix.length)        .split(/ +/g);    
 const command = args.shift().toLowerCase();   
 const commandFile = client.commands.get(command) || client.commands.get(client.aliases.get(command));
