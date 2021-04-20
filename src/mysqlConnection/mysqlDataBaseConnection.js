9c53140033fca597cdc13e5b8ba2c1cb823f2f8c
kayake <77542672+kayke981@users.noreply.github.com>
2021-04-20 12:00:26 -0300

    mysql ai pa cs

---
+++ src/mysqlConnection/mysqlDataBaseConnection.js
@@ -0,0 +1,36 @@
+module.exports = async (client) => { 
+const c = require('colors') 
+const mysql = require('mysql'); 
+let connection = mysql.createConnection({
+ host: 'ip', 
+port: 'porta', 
+user: 'usuário', 
+password: 'senha', 
+database: 'database' 
+}); 
+connection.connect(function(err) { 
+if (err) { 
+console.error(c.red('erro ao conectar ' + err.stack));
+ return; 
+} 
+console.log(c.green('[DATABASE MYSQL CONNECTED] conectado ao id ' + connection.threadId)); 
+client.mysql = connection; 
+}); 
+let poolCluster = mysql.createPoolCluster(); 
+poolCluster.add('NOME1', 'nome1Config'); 
+poolCluster.add('NOME2', 'nome2Config'); 
+poolCluster.getConnection('Wumpus', function(err, err) { 
+if(err) { 
+console.error(c.red('erro ao conectar ao cluster', err.stack)) 
+return; 
+} 
+console.log(c.green('[CLUSTER CONNECTED] cluster conectado')) 
+})
+poolCluster.getConnection('Hana', function(err, err) { 
+if(err) { 
+console.error(c.red('erro ao conectar ao cluster', err.stack)) 
+return; 
+} 
+console.log(c.green('[CLUSTER CONNECTED] cluster conectado')) 
+}) 
+}
