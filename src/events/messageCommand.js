b4bf6a18c979ee6f33fd78e7d492bebc11ba108a
kayake <77542672+kayke981@users.noreply.github.com>
2021-05-03 09:35:49 -0300

    ...

--- src/events/messageCommand.js
+++ src/events/messageCommand.js
@@ -4,6 +4,7 @@ const stringS = require('string-similarity');
 
 module.exports = (client) => {
 client.on('message', async (message) => { 
+const connection = client.mysql()
 if (message.author.bot) return;    
 if (message.channel.type == 'dm') return;
 if (!message.content.toLowerCase().startsWith(config.prefix)) return;
