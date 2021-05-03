545ec70433018102c485fad5efcd777736a7883a
kayake <77542672+kayke981@users.noreply.github.com>
2021-05-03 09:07:47 -0300

    atualizei pq tinha mt linha

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -43,47 +43,5 @@ channel.send({embed: embed })`, 2)
             console.log(err)
         }
     })
-    /*const newServer = new Discord.MessageEmbed()
-    .setAuthor(`Sai de um servidor (${guild.name}/${guild.id}`, guild.iconURL())
-    .setDescription(`**Nome do servidor**\n\`${guild.name}\`\n**ID**\n\`${guild.id}\`**Shard**\n\`${guild.shard.id}\`**Dono**\n(\`${guild.owner.tag}/${guild.owner.id}\`)\n**Total de membros**\n\`${guild.totalMembers}\``)
-    .setThumbnail(guild.iconURL())
-    
-    let channel = await client.channels.cache.get("818539587538386964")
-    if(!channel) return;
-    channel.send(newServer)*/
-    /*let icon = (!guilds.iconURL()?'https://cdn.discordapp.com/attachments/795130563916595270/838503065836584960/PSX_20210502_165304.jpg':guilds.iconURL())
-    
-    client.shard.broadcastEval(`let channel = this.channels.cache.get("id do canal")
-
-let embed = {
-color: "#b600ff",
-author: {
- name: \`Sai de um servidor | (${guilds.name}/${guilds.id})\`,
-icon_url: \`${icon}\`,
-},
-thumbnail: {
-url: \`${icon}\`,
-},
-fields: [
-{
-name: \`Nome:\`,
-value: \`(\\\`${guilds.name}/${guilds.id}\\\`)\`,
-},
-{
-name: \`Dono:\`,
-value: \`(\\\`${guilds.owner.user.tag}/${guilds.owner.id}\\\`)\`,
-},
-{
-name: \`Total de membros\`,
-value: \`${guilds.memberCount}\`,
-},
-],
-timestamp: new Date(),
-footer: {
-text: \`${guilds.id}\`, 
-icon_url: \`${icon}\`, 	
-},
-}
-channel.send({embed: embed })`, 2)*/
     });
 };
