10009634219d9cd73c8c42bb3f86b93a6bb29aa6
kayake <77542672+kayke981@users.noreply.github.com>
2021-05-02 22:18:22 -0300

    pronto

--- src/events/messageCommand.js
+++ src/events/messageCommand.js
@@ -30,6 +30,59 @@ const commandFile = client.commands.get(command) || client.commands.get(client.a
             if(cmds.includes('`')) return;
             if(!commandFile) return message.channel.send(`o comando \`${cmds}\` não existe, mas você não quis dizer \`${bestMatch.target}\``)
 
+if (!client.cooldowns.has(commandFile.name)) {
+            client.cooldowns.set(commandFile.name, new Discord.Collection());
+
+        }
+
+        const now = Date.now(); 
+
+        const timestamps = client.cooldowns.get(commandFile.name); 
+
+        const cooldownAmoun = (5) * 1000;
+        let cooldownAmount = (timestamps.has(message.author.id)? (Number(cooldownAmoun)) + 4000:cooldownAmoun)
+            
+
+        if (timestamps.has(message.author.id)) { 
+            
+          const expirationTime = timestamps.get(message.author.id) + cooldownAmount; 
+
+          if (now < expirationTime) { 
+              
+              if(client.gatilhos.has(message.author.id)) {
+                  let time = client.gatilhos.get(message.author.id)
+                 if(time > 35) {
+                     
+                     client.gatilhos.delete(message.author.id)
+                     
+                     await client.db.set(`motivo-${message.author.id}`, 'fez uma raid no bot para tentar derruba-la')
+                await client.db.push(`blacklist`, message.author.id)
+                
+                let motivo = await client.db.get(`motivo-${message.author.id}`)
+                return message.author.send(`**Você foi banida(o) de me usar, provalvelmente você fez algo, como spamar comandos meus e etc..., o motivo do ban foi \`${motivo}\`\nE sim, foi permanente o ban**`)
+                     
+
+                 }
+              }
+              
+              let quantidade = (client.gatilhos.has(message.author.id)?client.gatilhos.get(message.author.id):0)
+              
+              client.gatilhos.set(message.author.id, quantidade + 1)
+
+            const timeLeft = (expirationTime - now) / 1000; 
+
+          return  message.inlineReply(`😑 **|** você está usando comando rápido demais, espere ${timeLeft.toFixed(1)} segundos`)
+              }
+        }
+ timestamps.set(message.author.id, now);      
+                
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        setTimeout(() => {
+
+            client.gatilhos.delete(message.author.id)
+        }, 120000);
+        
+
 
 if(commandFile) {
     
