506c7cc58bab2cab300600f22a3a393aa69ca228
kayake <77542672+kayke981@users.noreply.github.com>
2021-05-03 09:25:32 -0300

    mysql atualizada

--- src/mysqlConnection/mysqlDataBaseConnection.js
+++ src/mysqlConnection/mysqlDataBaseConnection.js
@@ -1,36 +1,27 @@
-module.exports = async (client) => { 
-const c = require('colors') 
-const mysql = require('mysql'); 
-let connection = mysql.createConnection({
- host: 'ip', 
-port: 'porta', 
-user: 'usuário', 
-password: 'senha', 
-database: 'database' 
-}); 
-connection.connect(function(err) { 
-if (err) { 
-console.error(c.red('erro ao conectar ' + err.stack));
- return; 
-} 
-console.log(c.green('[DATABASE MYSQL CONNECTED] conectado ao id ' + connection.threadId)); 
-client.mysql = connection; 
-}); 
-let poolCluster = mysql.createPoolCluster(); 
-poolCluster.add('NOME1', 'nome1Config'); 
-poolCluster.add('NOME2', 'nome2Config'); 
-poolCluster.getConnection('Wumpus', function(err, err) { 
-if(err) { 
-console.error(c.red('erro ao conectar ao cluster', err.stack)) 
-return; 
-} 
-console.log(c.green('[CLUSTER CONNECTED] cluster conectado')) 
-})
-poolCluster.getConnection('NOME', function(err, err) { 
-if(err) { 
-console.error(c.red('erro ao conectar ao cluster', err.stack)) 
-return; 
-} 
-console.log(c.green('[CLUSTER CONNECTED] cluster conectado')) 
-}) 
+const c = require('colors')
+const mysql = require('mysql');
+
+var mySqlConnection = () => {
+
+  const connection = mysql.createConnection({
+        host: 'ip',
+
+        port: 'porta',
+
+        user: 'user',
+
+        password: 'senha',
+
+        database: 'db'
+  })
+  connection.connect(function(err) {
+    if (err) {
+      console.error('[ DataBase - MySql ] Ocorreu um erro ao tentar se conectar: \n\n' + err.stack);
+      return;
+    }
+  });
+  return connection;
+}
+module.exports = async (client) => {
+    client.mysql = mySqlConnection
 }
