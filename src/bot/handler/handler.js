1a72b160f30379a4d31dce7721b9cde309c0f89e
kayake <77542672+kayke981@users.noreply.github.com>
2021-06-15 15:21:15 -0300

    Add files via upload

---
+++ src/bot/handler/handler.js
@@ -0,0 +1,61 @@
+const {readdir, readdirSync} = require('fs');
+const Asciitable = require('ascii-table'); 
+let cmds = [] 
+const {cyan} = require('colors')
+exports.loadCommand = function (commands, aliases, log) {
+    
+readdir("./src/commands/", (err, files) => { 
+
+let jsfile = files.filter(f => f.split(".").pop() === "js");
+ console.log('carregando mais de ' + jsfile.length + ' comandos!!!') 
+    if (jsfile.length <= 0) { 
+return console.log("[LOGS] não acho comandos :|"); 
+} 
+jsfile.forEach((f, i) => { 
+let pull = require(`../../commands/${f}`); 
+
+commands.set(pull.config.name, pull); pull.config.aliases.forEach(alias => { 
+aliases.set(alias, pull.config.name); 
+}); 
+
+console.log(`${log.replace(/{cmd}/g, f).replace(/{number}/g, i += 1)}`.green);
+ cmds.push({name: pull.config.name, n: i , ali: pull.config.aliases.join(',')}); 
+}); 
+const table = new Asciitable('comandos') 
+table.setHeading('número', 'nome', 'aliases') 
+    
+cmds.forEach(i => { 
+table.addRow(i.n, i.name, i.ali) 
+}) 
+   console.log(cyan(table.toString())) 
+}); 
+}
+exports.getInfo = function() {
+const files = readdirSync("../../commands")
+let jsfile = files.filter(f => f.split(".").pop() === "js");
+let frase = []; 
+jsfile.forEach((f, i) => { 
+let pull = require(`../commands/${f}`)
+
+ frase.push(`${pull.config.category} - ${pull.config.name} | ${pull.config.aliases.join('|')}`) 
+}) 
+    return frase;
+}
+
+exports.getCategory = function(category) {
+
+ let arr = [];
+const files = readdirSync("../../commands")
+let jsfile = files.filter(f => f.split(".").pop() === "js");
+jsfile.forEach((f, i) => { 
+
+const pull = require(`../../commands/${f}`)
+ let c = pull.config.category;
+ if(c !== category) return; 
+else 
+{ 
+arr.push(`\`${pull.config.name}\``) 
+}
+})
+return arr;
+}
